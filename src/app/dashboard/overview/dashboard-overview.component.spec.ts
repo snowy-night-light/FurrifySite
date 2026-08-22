81895ca8547d29cb4f8c4997c26e3616f340390a
Snowy <skyterix1991@protonmail.com>
2026-08-22 21:22:19 +0200

    Tests fixed

--- src/app/dashboard/overview/dashboard-overview.component.spec.ts
+++ src/app/dashboard/overview/dashboard-overview.component.spec.ts
@@ -2,13 +2,33 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardOverviewComponent } from './dashboard-overview.component';
 
+import { provideTranslateService } from '@ngx-translate/core';
+import { AuthService } from '../../../shared/service/auth.service';
+import { UserStatisticsV1RestControllerService } from '../../../openapi/generated/storage';
+import { provideHttpClient } from '@angular/common/http';
+import { signal } from '@angular/core';
+
+import { of } from 'rxjs';
+
 describe('OverviewComponent', () => {
   let component: DashboardOverviewComponent;
   let fixture: ComponentFixture<DashboardOverviewComponent>;
 
   beforeEach(async () => {
+    (window as any).ResizeObserver = class {
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+    };
+
     await TestBed.configureTestingModule({
-      imports: [DashboardOverviewComponent]
+      imports: [DashboardOverviewComponent],
+      providers: [
+          provideHttpClient(),
+          provideTranslateService(),
+          { provide: AuthService, useValue: { getUserId: () => 'test-id' } },
+          { provide: UserStatisticsV1RestControllerService, useValue: { getUserStatistics: () => of({}) } }
+      ]
     })
     .compileComponents();
 
